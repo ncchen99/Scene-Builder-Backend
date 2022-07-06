3ee3032bd0c768876bc2c3f913a479caec5404a7
ncchen99 <ncchen99@gmail.com>
2022-07-06 11:41:30 +0800

    🥺

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-
-from flask import Flask, request, render_template, redirect, url_for, jsonify
+import json
+import os
+from flask import Flask, request, render_template, redirect, send_from_directory, url_for, jsonify
 from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ABC'
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -12,18 +14,21 @@ CORS(app)
 # @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def submit():
     if request.method == 'POST':
-        data = request.data
+        data = json.loads(request.data.decode('utf-8'))
+        file = open("data/data.json", "w", encoding='utf-8')
+        json.dump(data, file, indent=4, ensure_ascii=True)
+        file.close()
         print("post : data => ", data)
-        response = jsonify({})
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        response = jsonify({"ok": True})
+
         return response
 
 
-@app.route('/', methods=['POST', 'GET'])
-def root():
-    response = jsonify({})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+@app.route('/data/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    print(os.path.join(os.getcwd(), "data/"))
+    print(filename)
+    return send_from_directory("data", filename, as_attachment=True)
 
 
 if __name__ == '__main__':
